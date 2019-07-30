026d2d16efc839520097c7d8e5c9f986b20a5253
Alec Gillis <gillisa3@msu.edu>
2019-07-30 18:55:03 -0400

    Base program with no logic.

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -1,13 +1,26 @@
 '''
- # -*- coding: utf-8 -*-
 Pomodoro Technique CLI
 '''
-import sys
 import click
 
-def main():
-	print('in main')
+@click.command()
+#@click.option('--init')
+#@click.option('--list'
+#@click.option('--add')
+#@click.option('--remove')
+#@click.option('--start')
+
+if __name__ == '__main__':
+    print('in main')
+    hello()
 
 
-if __name__ == "__main__":
-	main()
+#@click.option("--count", default=1, help="Number of greetings.")
+#@click.option("--name", prompt="Your name",
+#              help="The person to greet.")
+#def hello(count, name):
+#    """Simple program that greets NAME for a total of COUNT times."""
+#    for _ in range(count):
+#        click.echo("Hello, %s!" % name)
+
+
